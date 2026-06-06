010e0e9f402a3684d779b4176b3043739e75522e
Sefa <sefaguemus@valuetics.com>
2026-06-06 14:31:06 +0300

    added example of "for - loop"

---
+++ OOP/Basic/for-loop.py
@@ -0,0 +1,15 @@
+# “5 kere döngü yap”
+for _ in range(5):
+    pass
+
+# listenin içindeki HER ELEMANI tek tek al
+for obj in liste:
+     pass
+
+"""
+👉 1’den başla
+👉 360’a kadar git
+👉 3’er 3’er artır
+"""
+for x in range(1, 360, 3):
+    pass
