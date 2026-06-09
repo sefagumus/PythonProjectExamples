e40373cc477eeaa2e2878f7bee5a32588f7f433c
Sefa <sefaguemus@valuetics.com>
2026-06-09 04:34:05 +0300

    added BubbleSort Draft

---
+++ OOP/Draft/BubbleSort.py
@@ -0,0 +1,8 @@
+def BubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+print(BubbleSort([7,3,9,12,5,4,52]))
